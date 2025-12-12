6b0fa23bc5f00c0cc66215a8f923d83cb2de1a45
Aaron Tanguma <the_guma@yahoo.com>
2025-12-11 19:21:20 -0700

    demonstrated division and modulus operations

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -8,17 +8,23 @@
             int a = 17; // initialized an integer variable
             int b = 4; // initialized an integer variable
 
-            int sum = a + b;
-            int difference = a - b;
-            int product = a * b;
+            int sum = a + b; // initialized sum variable to demonstrate addition operator
+            int difference = a - b; // intialized difference variable to demonstrate subtraction operator
+            int product = a * b; //initialized product variable to demonstrate multiplication variable
 
-            Console.WriteLine(sum);
-            Console.WriteLine(difference);
-            Console.WriteLine(product);
+            Console.WriteLine(sum); // write sum to console
+            Console.WriteLine(difference); // write difference to console
+            Console.WriteLine(product); // write product to console
             
             // division & modulus operations
-            int c = 32;
-            int d = 6;
+            int c = 32; // initialized an int variable
+            int d = 6; // initialized an int variable
+
+            int quotient = c / d; // initialized quotient variable to demonstrate division operator
+            int remainder = c % d; // initialized remainder variable to demonstrate modulus operator
+
+            Console.WriteLine(quotient); // write quotient to console
+            Console.WriteLine(remainder); // write remainder to console
         }
     }
 }
